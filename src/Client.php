9fb95628717ae76f45caba63a7c4d679856a652b
Bogdan Klimchenko <klim@office.meta.ua>
2016-04-20 13:26:45 +0300

    Filter src through php-cs-fixer

--- src/Client.php
+++ src/Client.php
@@ -41,6 +41,7 @@ class Client
                 $obj->fillData($contact);
                 $ret[] = $obj;
             }
+
             return $ret;
         }
         throw new \Exception(json_decode($res->getBody(), true)['error'], $res->getStatusCode());
@@ -119,42 +120,42 @@ class Client
             $obj->setPhones($aPhones);
             $obj->setNotes($aNotes);
             $obj->setRelations($aRelations);
+
             return $obj;
         }
         throw new \Exception(json_decode($res->getBody(), true)['error'], $res->getStatusCode());
     }
-    
-    public function addContact($accountName, $bookName, $contact, $campaignId = null) 
+
+    public function addContact($accountName, $bookName, $contact, $campaignId = null)
     {
-        $url = $this->baseUrl.'/api/v1/'.$accountName.'/'.$bookName.'/contact/add' ;
-        $data = $contact->retriveData() ;
-        
-        if(isset($campaignId)) {
-            $data['campaign_id'] = $campaignId ;
+        $url = $this->baseUrl.'/api/v1/'.$accountName.'/'.$bookName.'/contact/add';
+        $data = $contact->retriveData();
+
+        if (isset($campaignId)) {
+            $data['campaign_id'] = $campaignId;
         }
-        
+
         $res = $this->httpClient->post($url, [
             'auth' => [$this->username, $this->password],
-            'body' => json_encode($data)
+            'body' => json_encode($data),
         ]);
-        
-        if($res->getStatusCode() == 200) {
-            return true ;
+
+        if ($res->getStatusCode() == 200) {
+            return true;
         } else {
             throw new \Exception(json_decode($res->getBody(), true)['error'], $res->getStatusCode());
         }
     }
-    
-    public function addContactToCampaign($accountName, $bookName, $contactId, $campaignId) 
+
+    public function addContactToCampaign($accountName, $bookName, $contactId, $campaignId)
     {
-        $url = $this->baseUrl.'/api/v1/'.$accountName.'/'.$bookName.'/campaign/'.$campaignId.'/addcontact/'.$contactId ;
+        $url = $this->baseUrl.'/api/v1/'.$accountName.'/'.$bookName.'/campaign/'.$campaignId.'/addcontact/'.$contactId;
         $res = $this->httpClient->get($url, ['auth' => [$this->username, $this->password]]);
 
-        if($res->getStatusCode() == 200) {
-            return true ;
+        if ($res->getStatusCode() == 200) {
+            return true;
         } else {
             throw new \Exception(json_decode($res->getBody(), true)['error'], $res->getStatusCode());
         }
     }
-    
 }
